577f57bf47617bc4bfdae9dd8e21056a52f4d5cf
Nicole <mokus80@gmail.com>
2014-04-04 15:16:11 +0200

    change model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404122057) do
+ActiveRecord::Schema.define(version: 20140404130956) do
 
   create_table "messages", force: true do |t|
-    t.string   "text"
+    t.string   "user"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
