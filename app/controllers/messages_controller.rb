abfc3dbe754892de0a3a7745282e7bb48bca1f74
Nicole <mokus80@gmail.com>
2014-04-04 17:13:00 +0200

    changes in routes and controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,16 @@ class MessagesController < ApplicationController
 	
 	def index
 		last_checket_at = params[:last_checked_at]
-		@messages = Message.all.reverse
-		#render json: @messages
+		@messages = Message.all
 	end
 
 	def create
 		Message.create(params[:content])
 		redirect_to '/messages'
+	end
+
+	def test
+		@test = @messages = Message.all
+		render json: @test
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ChatApp::Application.routes.draw do
 
   resources :messages
+
+  get '/test', to: 'messages#test'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
