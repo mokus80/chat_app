f79848580fed861fd19819d5c157c179f8f3229f
Nicole <mokus80@gmail.com>
2014-04-05 14:01:29 +0200

    fix precompilation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 
 //$("#chat").append("<%= j render(@message) %>");
 //$("#new_message")[0].reset();
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ ChatApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.enabled = true
+  config.assets.compile = true
+  config.assets.initialize_on_precompile = true
+  config.assets.precompile += ['application.css']
 end
