9b8e9e9ce1a4f22ae33feaf31c7ddb95fcf34144
Nicole <mokus80@gmail.com>
2014-04-07 10:06:45 +0200

    fix what i broke

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,12 @@
 class MessagesController < ApplicationController
 	
 	def index
-		#last_checket_at = params[:last_checked_at]
 		@messages = Message.limit(10).order('created_at desc')
 		@message = Message.new
 	end
 
 	def create
-		message = Message.new(params[:message].permit(:content))
+		message = Message.create(params[:message].permit(:content))
 		if current_user.present?
 			message.user_id = current_user.id
 		end
